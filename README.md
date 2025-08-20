0be845ddef395d5b3d0ce300e2c986d9acc4642a
vema15 <140360041+vema15@users.noreply.github.com>
2025-08-19 21:02:46 -0700

    See README.md

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Loan-Portfolio-Pricing-Model-Set
+
+ABOUT
+The Loan Portfolio Pricing Model Set is (will be) as suite of pricing models for a variety of loan types. Each model will evaluate a raw loan tape and output the portfolio's weighted average price (over)under par and its associated dollar price. Each model will consider the intricacies of the loan types' legal, financial, and economic traits in order to produce the most accurate projections.
+
+**DISCLAIMER: None of the outputs produced by these models should be used as financial advice. Please be advised that past results are not indicative of future performance***
+
+CURRENT SUITE 
+
+no-effect-amortizing PRICING MODEL
+
+The No-Effect Pricing model determines the price of an amortizing installment loan based on:
+    a. An estimated Conditional Prepayment Rate (CPR) Curve
+    b. An estimated Annual Charge Off (ACO) Curve
+    c. A target yield (IRR)
+
+Current Static Characteristics/Assumptions
+1) 30/360 Accrual Basis
+2) Payment Scheduling: No Effect (Prepayments do not result in a change in the scheduled payment)
+3) CPR/ACO Curves
+
+UPDATES
+
+0.0.1: ALPHA 1
+    Added the no-effect-amortizing Pricing Model
+    Notes:
+    Currently working on how to optimize loading times
--- models/no-eff-amort/main.py
+++ models/no-eff-amort/main.py
@@ -1,12 +1,43 @@
 import pandas as pd
 import datetime
 
+# XIRR function is based on the IRR mathematic formula
+def xirr_cal(dyn_cf_list: list):
+    temp_irr_list = []
+    temp_irr = .0001
+    for i in range(100000):
+        disc_cfs = []
+        per = 0
+        for cash_flow in dyn_cf_list:
+            if per == 1:
+                disc_cfs.append(cash_flow[1])
+            else:
+                disc_cf = cash_flow[1] / (1 + temp_irr/12)**per
+                disc_cfs.append(disc_cf)
+            per += 1
+        if disc_cfs[0] + sum(disc_cfs[1:-1]) > 1:
+            temp_irr += 0.0001
+            temp_add_irr= temp_irr + .0001
+            temp_irr_list.append(temp_add_irr)  
+        elif disc_cfs[0] + sum(disc_cfs[1:-1]) < 1 and len(temp_irr_list) >2:
+            temp_irr -= .0001
+            temp_sub_irr = temp_irr - .0001
+            temp_irr_list.append(temp_sub_irr)
+        else:
+            print(temp_irr)
+        if len(temp_irr_list) > 30 and temp_irr_list[-1] == temp_irr_list[-3]:
+            return temp_irr
+            break
+            
+
 test_mode = True
 
-# Is interchangeable w/ .xlsx files
+# Can be interchangeable w/ .xlsx files
 tape_path = "models/no-eff-amort/loan_tape_amort_ex.csv"
 tape_df = pd.DataFrame(pd.read_csv(tape_path))
 tape_df.reset_index()
+
+#Define dataframe column names
 
 col_names = {}
 required_columns = ["Loan ID", "Original Loan Amount", "Interest Rate", "Original Term", "Funding Date", "UPB", "Payments"]
@@ -24,6 +55,7 @@ else:
 
 port_cf = {}
 
+#Transaction detail entry
 if test_mode == False:
     settlement_date = input("Please enter the estimated or actual purchase settlement date: ")
 else:
@@ -35,14 +67,15 @@ for index, row in tape_df.iterrows():
     smm_curve = [0.01 for _ in range(row[col_names["Original Term"]]+1)]
     mco_curve = [0.001 for _ in range(row[col_names["Original Term"]]+1)]
 
+    #Cash Flow Model / Amort. Table
     loan_cash_flows = []
     loan_cash_flows.append([settlement_date, -row[col_names["UPB"]]])
     for period in range(row[col_names["Original Term"]]+1):
         if period == 0:
             continue
         curr_pmt_period = period + row[col_names["Payments"]]
-        #30/360 accrual convention used. Installment loan DOES NOT change modify sched. pmt due to prepayment unless it is the last payment
 
+        #30/360 accrual convention used. Installment loan DOES NOT change modify sched. pmt due to prepayment unless it is the last payment
         sched_pmt = row[col_names["Original Loan Amount"]] * ((row[col_names["Interest Rate"]]/12)) / (1 - (1 + (row[col_names["Interest Rate"]]/12))**(-row[col_names["Original Term"]])) 
         sched_impt = (row[col_names["Interest Rate"]]/12) * (row_bal)
         sched_ppmt = sched_pmt - sched_impt
@@ -64,35 +97,49 @@ for index, row in tape_df.iterrows():
             break
     port_cf[row[col_names["Loan ID"]]] = loan_cash_flows
 
-targ_yield = input("Input target yield as decimal: ")
+#Enter the intended deal yield
+targ_yield = float(input("Input target yield as decimal: "))
 loan_prices = {}
+print("====LOADING====")
 for loan_id, cash_flows in port_cf.items():
-    par_price = -cash_flows[0][1]
-    temp_irr_list = []
-    temp_irr = .0001
-    irr = 0
-    for i in range(100000):
-        disc_cfs = []
-        per = 0
-        for cash_flow in cash_flows:
-            if per == 1:
-                disc_cfs.append(cash_flow[1])
-            else:
-                disc_cf = cash_flow[1] / (1 + temp_irr/12)**per
-                disc_cfs.append(disc_cf)
-            per += 1
-        
-        if disc_cfs[0] + sum(disc_cfs[1:-1]) > 1:
-            temp_irr += 0.0001
-            temp_add_irr= temp_irr + .0001
-            temp_irr_list.append(temp_add_irr)  
-        elif disc_cfs[0] + sum(disc_cfs[1:-1]) < 1 and len(temp_irr_list) >2:
-            temp_irr -= .0001
-            temp_sub_irr = temp_irr - .0001
-            temp_irr_list.append(temp_sub_irr)
-        else:
-            print(temp_irr)
-        if len(temp_irr_list) > 30 and temp_irr_list[-1] == temp_irr_list[-3]:
-            irr = temp_irr
+    irr = 0 
+    amt_paid = 0
+    moe = 0
+
+    irr_tracker = []
+    dyn_cf = cash_flows[0:-1]
+    # Custom IRR function from scratch
+    for i in range(10000):
+        irr = xirr_cal(dyn_cf)
+        if irr < targ_yield:
+            dyn_cf[0][1] *= (1-0.0001)
+        elif irr > targ_yield:
+            dyn_cf[0][1] *= (1+0.0001)
+        if len(irr_tracker)>= 3 and (int(irr_tracker[-1]) == int(irr_tracker[-3])):
+            amt_paid = -dyn_cf[0][1]
+            moe = (float(targ_yield) - float(irr))
             break
-    print(irr)       
+        irr_tracker.append(dyn_cf[0][1]) 
+    loan_UPB = float(tape_df.loc[tape_df['Loan ID'] == loan_id]["UPB"].iloc[0])
+    price_over_under_par = (amt_paid/loan_UPB)*100
+    loan_prices[loan_id] = [loan_UPB, price_over_under_par, irr]
+
+sumprod = 0
+sum = 0
+
+
+for key, value in loan_prices.items():
+    print(f'{key}: UPB - {value[0]} | Price - {value[1]} | Dollar Price Paid - {value[0] * (value[1]/100)}')
+    sumprod += (value[0] * (value[1]/100))  
+    sum += value[0]
+
+w_avg_price = (sumprod/sum)*100
+port_price = sumprod
+print(f'\nWeighted Average Price - {w_avg_price}\nTotal Portfolio Price - {port_price}')
+    
+        
+   
+
+
+
+    
